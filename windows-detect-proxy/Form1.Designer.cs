4d98a87fdbc770a9d168cc7e84c44bf75304174e
bergwerkgis <wb@bergwerk-gis.at>
2015-06-23 10:33:13 +0200

    initial version

---
+++ windows-detect-proxy/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace windows_detect_proxy {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing ) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.IDC_tbUrl = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.IDC_bDetect = new System.Windows.Forms.Button();
+			this.IDC_lvInfo = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.IDC_eExit = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.IDC_bCopyInfo = new System.Windows.Forms.Button();
+			this.IDC_tbDefaultWebProxy = new System.Windows.Forms.TextBox();
+			this.IDC_tbSystemWebProxy = new System.Windows.Forms.TextBox();
+			this.IDC_tbRegistry = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// IDC_tbUrl
+			// 
+			this.IDC_tbUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.IDC_tbUrl.Location = new System.Drawing.Point(12, 25);
+			this.IDC_tbUrl.Name = "IDC_tbUrl";
+			this.IDC_tbUrl.Size = new System.Drawing.Size(557, 20);
+			this.IDC_tbUrl.TabIndex = 0;
+			this.IDC_tbUrl.Text = "https://a.tiles.mapbox.com/";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(63, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Target URL";
+			// 
+			// IDC_bDetect
+			// 
+			this.IDC_bDetect.Location = new System.Drawing.Point(12, 51);
+			this.IDC_bDetect.Name = "IDC_bDetect";
+			this.IDC_bDetect.Size = new System.Drawing.Size(134, 23);
+			this.IDC_bDetect.TabIndex = 2;
+			this.IDC_bDetect.Text = "detect";
+			this.IDC_bDetect.UseVisualStyleBackColor = true;
+			this.IDC_bDetect.Click += new System.EventHandler(this.IDC_bDetect_Click);
+			// 
+			// IDC_lvInfo
+			// 
+			this.IDC_lvInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.IDC_lvInfo.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+			this.IDC_lvInfo.Location = new System.Drawing.Point(12, 274);
+			this.IDC_lvInfo.Name = "IDC_lvInfo";
+			this.IDC_lvInfo.Size = new System.Drawing.Size(557, 150);
+			this.IDC_lvInfo.TabIndex = 3;
+			this.IDC_lvInfo.UseCompatibleStateImageBehavior = false;
+			this.IDC_lvInfo.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Info";
+			this.columnHeader1.Width = 500;
+			// 
+			// IDC_eExit
+			// 
+			this.IDC_eExit.Location = new System.Drawing.Point(152, 51);
+			this.IDC_eExit.Name = "IDC_eExit";
+			this.IDC_eExit.Size = new System.Drawing.Size(75, 23);
+			this.IDC_eExit.TabIndex = 4;
+			this.IDC_eExit.Text = "exit";
+			this.IDC_eExit.UseVisualStyleBackColor = true;
+			this.IDC_eExit.Click += new System.EventHandler(this.IDC_eExit_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.IDC_tbRegistry);
+			this.groupBox1.Controls.Add(this.IDC_tbSystemWebProxy);
+			this.groupBox1.Controls.Add(this.IDC_tbDefaultWebProxy);
+			this.groupBox1.Location = new System.Drawing.Point(12, 93);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(557, 146);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = " detexted proxy settings ";
+			// 
+			// IDC_bCopyInfo
+			// 
+			this.IDC_bCopyInfo.Location = new System.Drawing.Point(12, 245);
+			this.IDC_bCopyInfo.Name = "IDC_bCopyInfo";
+			this.IDC_bCopyInfo.Size = new System.Drawing.Size(75, 23);
+			this.IDC_bCopyInfo.TabIndex = 6;
+			this.IDC_bCopyInfo.Text = "copy info";
+			this.IDC_bCopyInfo.UseVisualStyleBackColor = true;
+			this.IDC_bCopyInfo.Click += new System.EventHandler(this.IDC_bCopyInfo_Click);
+			// 
+			// IDC_tbDefaultWebProxy
+			// 
+			this.IDC_tbDefaultWebProxy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.IDC_tbDefaultWebProxy.Location = new System.Drawing.Point(129, 34);
+			this.IDC_tbDefaultWebProxy.Name = "IDC_tbDefaultWebProxy";
+			this.IDC_tbDefaultWebProxy.Size = new System.Drawing.Size(422, 20);
+			this.IDC_tbDefaultWebProxy.TabIndex = 10;
+			// 
+			// IDC_tbSystemWebProxy
+			// 
+			this.IDC_tbSystemWebProxy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.IDC_tbSystemWebProxy.Location = new System.Drawing.Point(129, 60);
+			this.IDC_tbSystemWebProxy.Name = "IDC_tbSystemWebProxy";
+			this.IDC_tbSystemWebProxy.Size = new System.Drawing.Size(422, 20);
+			this.IDC_tbSystemWebProxy.TabIndex = 11;
+			// 
+			// IDC_tbRegistry
+			// 
+			this.IDC_tbRegistry.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.IDC_tbRegistry.Location = new System.Drawing.Point(129, 86);
+			this.IDC_tbRegistry.Name = "IDC_tbRegistry";
+			this.IDC_tbRegistry.Size = new System.Drawing.Size(422, 20);
+			this.IDC_tbRegistry.TabIndex = 12;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 89);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(45, 13);
+			this.label2.TabIndex = 13;
+			this.label2.Text = "Registry";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 63);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(90, 13);
+			this.label3.TabIndex = 14;
+			this.label3.Text = "SystemWebProxy";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 37);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(90, 13);
+			this.label4.TabIndex = 15;
+			this.label4.Text = "DefaultWebProxy";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(581, 436);
+			this.Controls.Add(this.IDC_bCopyInfo);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.IDC_eExit);
+			this.Controls.Add(this.IDC_lvInfo);
+			this.Controls.Add(this.IDC_bDetect);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.IDC_tbUrl);
+			this.Name = "Form1";
+			this.Text = "detect proxy";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox IDC_tbUrl;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button IDC_bDetect;
+		private System.Windows.Forms.ListView IDC_lvInfo;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.Button IDC_eExit;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button IDC_bCopyInfo;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox IDC_tbRegistry;
+		private System.Windows.Forms.TextBox IDC_tbSystemWebProxy;
+		private System.Windows.Forms.TextBox IDC_tbDefaultWebProxy;
+	}
+}
+
---
+++ windows-detect-proxy/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+namespace windows_detect_proxy {
+
+
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+
+		private string _TargetUrl;
+		private Uri _TargetUri;
+
+
+		private void IDC_eExit_Click( object sender, EventArgs e ) {
+			this.Close();
+		}
+
+
+		private void IDC_bDetect_Click( object sender, EventArgs e ) {
+
+			if (string.IsNullOrWhiteSpace( IDC_tbUrl.Text )) {
+				MessageBox.Show( "no target URL", "error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+				return;
+			}
+			_TargetUrl = IDC_tbUrl.Text;
+			if (!_TargetUrl.EndsWith( "/" )) { _TargetUrl += "/"; }
+			_TargetUri = new Uri( _TargetUrl );
+
+			IDC_lvInfo.Items.Clear();
+			IDC_tbDefaultWebProxy.Text = string.Empty;
+			IDC_tbSystemWebProxy.Text = string.Empty;
+			IDC_tbRegistry.Text = string.Empty;
+
+			try {
+				Cursor = Cursors.WaitCursor;
+				webRequestDefaultWebProxy();
+				webRequestGetSystemWebProxy();
+				readRegistry();
+			}
+			finally {
+				Cursor = Cursors.Default;
+			}
+		}
+
+
+
+		private void webRequestDefaultWebProxy() {
+
+			try {
+				log( "== testing WebRequest.DefaultWebProxy ==" );
+				IWebProxy proxy = WebRequest.DefaultWebProxy;
+				//TODO: how to get credentials???
+				Uri proxyUri = proxy.GetProxy( _TargetUri );
+				string proxyUrl = proxyUri.AbsoluteUri;
+				if (proxyUrl.Equals( _TargetUrl, StringComparison.OrdinalIgnoreCase )) {
+					log( "no proxy" );
+					IDC_tbDefaultWebProxy.Text = string.Empty;
+				} else {
+					log( proxyUrl );
+					IDC_tbDefaultWebProxy.Text = proxyUrl;
+				}
+				testRequest( proxy );
+			}
+			catch (Exception ex) {
+				log( ex.ToString(), true );
+			}
+		}
+
+		private void webRequestGetSystemWebProxy() {
+
+			try {
+				log( "== testing WebRequest.GetSystemWebProxy ==" );
+				IWebProxy proxy = WebRequest.GetSystemWebProxy();
+				Uri proxyUri = proxy.GetProxy( _TargetUri );
+				string proxyUrl = proxyUri.AbsoluteUri;
+				if( proxyUrl.Equals( _TargetUrl, StringComparison.OrdinalIgnoreCase )){
+					log( "no proxy" );
+					IDC_tbSystemWebProxy.Text=string.Empty;
+				}else{
+					log( proxyUrl );
+					IDC_tbSystemWebProxy.Text = proxyUrl;
+				}
+				testRequest( proxy );
+			}
+			catch (Exception ex) {
+				log( ex.ToString(), true );
+			}
+		}
+
+
+		private void readRegistry() {
+
+			try {
+				log( "== testing registry settings ==" );
+
+				WebProxy proxy = null;
+				IDC_tbRegistry.Text = string.Empty;
+
+				RegistryKey iSettings = Registry.
+					CurrentUser.
+					OpenSubKey( @"Software\Microsoft\Windows\CurrentVersion\Internet Settings" );
+
+				if (null == iSettings) {
+					log( "could not open registry key 'Internet Settings'", true );
+				} else {
+					object objEnable = iSettings.GetValue( "ProxyEnable" );
+					if (null == objEnable) {
+						log( "could not get value for registry key 'ProxyEnable'" );
+					} else {
+						int proxyEnable = Convert.ToInt32( objEnable );
+						if (0 == proxyEnable) {
+							log( "no proxy" );
+						} else {
+							log( "proxy enabled via registry" );
+							object objProxy = iSettings.GetValue( "ProxyServer" );
+							if (null == objProxy) {
+								log( "no proxy set", true );
+							} else {
+								string proxyUrl = objProxy.ToString();
+								log( proxyUrl );
+								IDC_tbRegistry.Text = proxyUrl;
+								//TODO parse responce
+								//proxy = new WebProxy( proxyUrl, true );
+							}
+						}
+					}
+				}
+
+				//testRequest( proxy );
+			}
+			catch (Exception ex) {
+				log( ex.Message, true );
+			}
+
+		}
+
+
+		private void testRequest( IWebProxy proxy ) {
+
+			try {
+				using (WebClient wc = new WebClient()) {
+					wc.Proxy = proxy;
+					log( "connecting ..." );
+					try {
+						log( "response: " + wc.DownloadString( _TargetUri ) );
+					}
+					catch (Exception ex2) {
+						log( "response: " + ex2.ToString(), true );
+					}
+				}
+			}
+			catch (Exception ex) {
+				log( ex.ToString(), true );
+			}
+		}
+
+
+		private void log( string msg, bool isError = false ) {
+
+			ListViewItem lvi = new ListViewItem( msg );
+			if (isError) {
+				lvi.BackColor = Color.OrangeRed;
+			}
+			IDC_lvInfo.Items.Add( lvi );
+		}
+
+		private void IDC_bCopyInfo_Click( object sender, EventArgs e ) {
+			MessageBox.Show( "not implemented" );
+		}
+
+
+	}
+}
